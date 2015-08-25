61be23f63ab0036f39c85dc997971b7917c0259d
Matt Ryan <ryan.mattc@gmail.com>
2015-08-24 22:39:47 -0400

    Removed unused import and made PEP8 compliant

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 import os
 from app import create_app, db
-from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+
 
 def make_shell_context():
     return dict(app=app, db=db)
